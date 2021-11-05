fc090870833124b0248406530cd005575abef762
Andrés Navarro S <31898794+AndresNS@users.noreply.github.com>
2021-11-05 18:05:24 -0300

    Agrega negocios de subasta de transporte

---
+++ maipogrande-web/src/java/Negocio/NegocioDetalleSubasta.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Conexion.Conexion;
+import DTO.DetalleSubasta;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Asus
+ */
+public class NegocioDetalleSubasta {
+    private Conexion con;
+
+    /**
+     * @return the con
+     */
+    public Conexion getCon() {
+        return con;
+    }
+
+    /**
+     * @param con the con to set
+     */
+    public void setCon(Conexion con) {
+        this.con = con;
+    }
+    
+    public void configurarConexion()
+    {
+        this.setCon(new Conexion());
+        this.getCon().setNombreBaseDeDatos("jdbc:oracle:thin:@localhost:1521:XE");
+        this.getCon().setNombreTabla("DETALLE_SUBASTA");
+        this.getCon().setCadenaConexion("oracle.jdbc.driver.OracleDriver");
+        this.getCon().setUsuario("maipogrande");
+        this.getCon().setPass("123");
+    } //Fin configurar
+    
+    public void insertarDetalleProcesoVenta(DetalleSubasta detalleSubasta)
+    {
+        this.configurarConexion();
+        this.getCon().setCadenaSQL("INSERT INTO " + this.getCon().getNombreTabla()+
+                                     " (ID_DETALLE_SUBASTA,FECHA_ESTIMADA_ENTREGA,CAPACIDAD_CARGA,REFRIGERACION,PESO_MAX,EXTRAS,PRECIO,ID_CABECERA_SUBASTA,ID_EMPRESA_TRANS) "
+                                             + "VALUES ("+
+                                    detalleSubasta.getIdDetalleSubasta()+ ",'"+
+                                    detalleSubasta.getFechaEstimadaEntrega()+ "','"+ 
+                                    detalleSubasta.getCapacidadCarga()+"','"+
+                                    detalleSubasta.getRefrigeracion()+"','"+
+                                    detalleSubasta.getPesoMaximo()+"','"+
+                                    detalleSubasta.getExtras()+"',"+
+                                    detalleSubasta.getPrecio()+","+
+                                    detalleSubasta.getIdCabeceraSubasta()+","+
+                                    detalleSubasta.getIdEmpresaTransporte()+");");
+        this.getCon().setEsSelect(false);
+        this.getCon().conectar();
+    }
+    
+    public ArrayList<DetalleSubasta> listarDetallesSubasta(int idCabeceraSubasta) {
+        ArrayList<DetalleSubasta> listaDetallesSubasta = new ArrayList<>();
+        this.configurarConexion();
+        this.getCon().setCadenaSQL("SELECT * FROM " +
+                                       this.getCon().getNombreTabla()+
+                                     " WHERE ID_CABECERA_SUBASTA = " +idCabeceraSubasta +";" );
+        this.getCon().setEsSelect(true);
+        this.getCon().conectar();
+
+        try
+        {
+           while(this.getCon().getDbResultSet().next())
+           {
+                DetalleSubasta detalleSubasta = new DetalleSubasta();
+                detalleSubasta.setIdDetalleSubasta(this.getCon().getDbResultSet().getInt("ID_DETALLE_SUBASTA"));
+                detalleSubasta.setFechaEstimadaEntrega(this.getCon().getDbResultSet().getDate("FECHA_ESTIMADA_ENTREGA"));
+                detalleSubasta.setCapacidadCarga(this.getCon().getDbResultSet().getString("CAPACIDAD_CARGA"));
+                detalleSubasta.setRefrigeracion(this.getCon().getDbResultSet().getString("REFRIGERACION"));
+                detalleSubasta.setPesoMaximo(this.getCon().getDbResultSet().getString("PESO_MAX"));
+                detalleSubasta.setExtras(this.getCon().getDbResultSet().getString("EXTRAS"));
+                detalleSubasta.setPrecio(this.getCon().getDbResultSet().getInt("PRECIO"));
+                detalleSubasta.setIdCabeceraSubasta(this.getCon().getDbResultSet().getInt("ID_CABECERA_SUBASTA"));
+                detalleSubasta.setIdEmpresaTransporte(this.getCon().getDbResultSet().getInt("ID_EMPRESA_TRANS"));
+
+                listaDetallesSubasta.add(detalleSubasta);
+           } //Fin while
+        }
+        catch(Exception ex)
+        {
+           JOptionPane.showMessageDialog(null, "Error SQL " + ex.getMessage());
+        } //Fin try cargar arraylist
+
+        return listaDetallesSubasta;
+    }
+}
